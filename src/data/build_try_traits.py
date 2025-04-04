1232f033d63df20cb04537d9abd1aa5e7d394951
Daniel Lusk <daniel.lusk@geosense.uni-freiburg.de>
2025-04-04 08:40:54 +0200

    Refactor transformation functions in build_try_traits.py
    - Reintroduced and reorganized transformation functions for filtering, log transformation, power transformation, and standardization of trait IDs.
    - Removed commented-out code and ensured clarity in the main function's data processing pipeline.
    - Improved code structure for better maintainability and readability.

--- src/data/build_try_traits.py
+++ src/data/build_try_traits.py
@@ -19,6 +19,79 @@ from src.utils.trait_utils import (
 )
 
 
+def main(cfg: ConfigBox = get_config()) -> None:
+    """Extract, clean, and get species mean trait values from TRY data."""
+    try_raw_dir = Path(cfg.raw_dir, cfg.trydb.raw.dir)
+    try_prep_dir = Path(cfg.interim_dir, cfg.trydb.interim.dir)
+
+    log.info("Extracting raw TRY traits data...")
+    with zipfile.ZipFile(try_raw_dir / cfg.trydb.raw.zip, "r") as zip_ref:  # noqa: SIM117
+        with zip_ref.open(cfg.trydb.raw.zipfile_csv) as zf:
+            if cfg.trydb.raw.zipfile_csv.endswith(".zip"):
+                with zipfile.ZipFile(zf, "r") as nested_zip_ref:  # noqa: SIM117
+                    with nested_zip_ref.open(nested_zip_ref.namelist()[0]) as csvfile:
+                        traits = pd.read_csv(csvfile, encoding="latin-1", index_col=0)
+            else:
+                traits = pd.read_csv(zf, encoding="latin-1", index_col=0)
+
+    log.info("Getting species mean trait values...")
+    traits = traits.pipe(standardize_trait_ids)
+    valid_traits = [f"X{t}" for t in cfg.datasets.Y.traits]
+    trait_cols = [
+        col for col in traits.columns if col.startswith("X") and col in valid_traits
+    ]
+
+    keep_cols = ["Species"] + trait_cols
+    mean_filt_traits = (
+        traits[keep_cols]
+        .pipe(_filter_if_specified, trait_cols, cfg.trydb.interim.quantile_range)
+        .pipe(clean_species_name, "Species", "speciesname")
+        .drop(columns=["Species"])
+        .groupby("speciesname")
+        .mean()
+    )
+
+    # Match with PFTs
+    log.info("Filtering by plant functional types...")
+    log.info("Loading PFTs...")
+    pft_path = Path(cfg.raw_dir, cfg.trydb.raw.pfts)
+    if pft_path.suffix == ".csv":
+        pfts = pd.read_csv(pft_path, encoding="latin-1")
+    elif pft_path.suffix == ".parquet":
+        pfts = pd.read_parquet(pft_path)
+    else:
+        raise ValueError(f"Unsupported PFT file format: {pft_path.suffix}")
+    pfts = (
+        pfts.drop(columns=["AccSpeciesID"])
+        .dropna(subset=["AccSpeciesName"])
+        .pipe(clean_species_name, "AccSpeciesName", "speciesname")
+        .drop(columns=["AccSpeciesName"])
+        .drop_duplicates(subset=["speciesname"])
+        .pipe(filter_pft, cfg.PFT)
+        .set_index("speciesname")
+    )
+
+    log.info("Matching trait data with filtered PFTs...")
+    mean_filt_traits = (
+        mean_filt_traits.join(pfts, how="inner")
+        .drop(columns=["pft"])
+        .pipe(
+            _transform,
+            cfg.trydb.interim.transform,
+            transformer_fn=try_prep_dir / cfg.trydb.interim.transformer_fn,
+        )
+        .reset_index()
+    )
+
+    log.info("Saving filtered and mean trait values...")
+    out_fn = try_prep_dir / cfg.trydb.interim.filtered
+    if out_fn.exists():
+        out_fn.unlink()
+    mean_filt_traits.to_parquet(
+        try_prep_dir / cfg.trydb.interim.filtered, index=False, compression="zstd"
+    )
+
+
 def _filter_if_specified(
     df: pd.DataFrame, trait_cols: list[str], quantile_range: tuple[float, float] | None
 ) -> pd.DataFrame:
@@ -38,7 +111,6 @@ def _log_transform_long_tails(df: pd.DataFrame, keep: list[str] | None) -> pd.Da
     long_tailed_cols = [
         col for col in trait_cols if int(get_trait_number_from_id(col)) not in keep
     ]
-    # new_cols = [f"{col}_ln" for col in long_tailed_cols]
 
     if not long_tailed_cols:
         log.info("No long-tailed columns to log-transform.")
@@ -85,80 +157,6 @@ def standardize_trait_ids(df: pd.DataFrame) -> pd.DataFrame:
     old_trait_cols = df.columns[df.columns.str.startswith("X")]
     new_trait_cols = [col.split(".")[1] for col in old_trait_cols if "." in col]
     return df.rename(columns=dict(zip(old_trait_cols, new_trait_cols)))
-
-
-def main(cfg: ConfigBox = get_config()) -> None:
-    """Extract, clean, and get species mean trait values from TRY data."""
-    try_raw_dir = Path(cfg.raw_dir, cfg.trydb.raw.dir)
-    try_prep_dir = Path(cfg.interim_dir, cfg.trydb.interim.dir)
-
-    log.info("Extracting raw TRY traits data...")
-    with zipfile.ZipFile(try_raw_dir / cfg.trydb.raw.zip, "r") as zip_ref:  # noqa: SIM117
-        with zip_ref.open(cfg.trydb.raw.zipfile_csv) as zf:
-            if cfg.trydb.raw.zipfile_csv.endswith(".zip"):
-                with zipfile.ZipFile(zf, "r") as nested_zip_ref:  # noqa: SIM117
-                    with nested_zip_ref.open(nested_zip_ref.namelist()[0]) as csvfile:
-                        traits = pd.read_csv(csvfile, encoding="latin-1", index_col=0)
-            else:
-                traits = pd.read_csv(zf, encoding="latin-1", index_col=0)
-
-    log.info("Getting species mean trait values...")
-    traits = traits.pipe(standardize_trait_ids)
-    valid_traits = [f"X{t}" for t in cfg.datasets.Y.traits]
-    trait_cols = [
-        col for col in traits.columns if col.startswith("X") and col in valid_traits
-    ]
-
-    keep_cols = ["Species"] + trait_cols
-    mean_filt_traits = (
-        traits[keep_cols]
-        .pipe(_filter_if_specified, trait_cols, cfg.trydb.interim.quantile_range)
-        # .pipe(standardize_trait_ids)
-        .pipe(clean_species_name, "Species", "speciesname")
-        .drop(columns=["Species"])
-        .groupby("speciesname")
-        .mean()
-    )
-
-    # Match with PFTs
-    log.info("Filtering by plant functional types...")
-    log.info("Loading PFTs...")
-    pft_path = Path(cfg.raw_dir, cfg.trydb.raw.pfts)
-    if pft_path.suffix == ".csv":
-        pfts = pd.read_csv(pft_path, encoding="latin-1")
-    elif pft_path.suffix == ".parquet":
-        pfts = pd.read_parquet(pft_path)
-    else:
-        raise ValueError(f"Unsupported PFT file format: {pft_path.suffix}")
-    pfts = (
-        pfts.drop(columns=["AccSpeciesID"])
-        .dropna(subset=["AccSpeciesName"])
-        .pipe(clean_species_name, "AccSpeciesName", "speciesname")
-        .drop(columns=["AccSpeciesName"])
-        .drop_duplicates(subset=["speciesname"])
-        .pipe(filter_pft, cfg.PFT)
-        .set_index("speciesname")
-    )
-
-    log.info("Matching trait data with filtered PFTs...")
-    mean_filt_traits = (
-        mean_filt_traits.join(pfts, how="inner")
-        .drop(columns=["pft"])
-        .pipe(
-            _transform,
-            cfg.trydb.interim.transform,
-            transformer_fn=try_prep_dir / cfg.trydb.interim.transformer_fn,
-        )
-        .reset_index()
-    )
-
-    log.info("Saving filtered and mean trait values...")
-    out_fn = try_prep_dir / cfg.trydb.interim.filtered
-    if out_fn.exists():
-        out_fn.unlink()
-    mean_filt_traits.to_parquet(
-        try_prep_dir / cfg.trydb.interim.filtered, index=False, compression="zstd"
-    )
 
 
 if __name__ == "__main__":
