48a209a15d1bc8aa375ec682f80cfabf2d66113b
Daniel Lusk <daniel.lusk@geosense.uni-freiburg.de>
2025-04-10 12:02:26 +0200

    Enhance container build process for Apptainer
    - Added logic to temporarily unset `APPTAINER_BINDPATH` during the container build to prevent failures due to missing bind targets.
    - Implemented the Apptainer build command with a force option, ensuring successful container creation.
    - Restored the original `APPTAINER_BINDPATH` after the build process, maintaining the environment's integrity.

--- stages/build_container.sh
+++ stages/build_container.sh
@@ -11,5 +11,19 @@ container_image=$5
 if [ "$container_type" = "docker" ]; then
     docker build -t "$container_name:$container_tag" -f "$container_def" .
 else
-    singularity build "$container_image" "$container_def"
+    # Temporarily backup and unset APPTAINER_BINDPATH if it's set, otherwise the
+    # build process will fail because the bind targets don't exist yet.
+    if [ -n "$APPTAINER_BINDPATH" ]; then
+        echo "Temporarily unsetting APPTAINER_BINDPATH for container build"
+        OLD_BINDPATH=$APPTAINER_BINDPATH
+        unset APPTAINER_BINDPATH
+    fi
+    
+    # Build with Apptainer
+    apptainer build --force "$container_image" "$container_def"
+    
+    # Restore APPTAINER_BINDPATH if it was previously set
+    if [ -n "$OLD_BINDPATH" ]; then
+        export APPTAINER_BINDPATH=$OLD_BINDPATH
+    fi
 fi 
