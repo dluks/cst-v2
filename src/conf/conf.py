57d066ac07a56edc3496a5b4b0140435d5e290f5
Daniel Lusk <daniel.lusk@posteo.net>
2025-09-19 13:35:37 +0200

    allow custom params file to be passed when getting config

--- src/conf/conf.py
+++ src/conf/conf.py
@@ -1,31 +1,40 @@
 """Parses a DVC params.yaml file to return a config object that can be used in other scripts."""
 
 import os
+from pathlib import Path
 
 import yaml
 from box import ConfigBox
 from dotenv import find_dotenv, load_dotenv
 
 
-def parse_params() -> dict:
+def parse_params(params_path: str | Path | None = None) -> dict:
     """
     Parse a DVC params.yaml file to return a config object that can be used in other scripts.
     Note that this requires a PROJECT_ROOT variable to be set as an environment variable
     in a local .env file.
     """
-    load_dotenv(find_dotenv())
-    with open(f"{os.environ['PROJECT_ROOT']}/params.yaml", "r", encoding="utf-8") as f:
+    if params_path is None:
+        load_dotenv(find_dotenv())
+        params_path = Path(os.environ["PROJECT_ROOT"]) / "params.yaml"
+
+    if not Path(params_path).exists():
+        raise FileNotFoundError(f"Params file not found at {params_path}")
+
+    with open(params_path, encoding="utf-8") as f:
         return yaml.safe_load(f.read())
 
 
-def get_config(subset: str | None = None) -> ConfigBox:
+def get_config(
+    subset: str | None = None, params_path: str | Path | None = None
+) -> ConfigBox:
     """
     Get a ConfigBox object from a dictionary.
     """
     if subset is not None:
-        return ConfigBox(parse_params())[subset]
+        return ConfigBox(parse_params(params_path))[subset]
 
-    return ConfigBox(parse_params())
+    return ConfigBox(parse_params(params_path))
 
 
 if __name__ == "__main__":
